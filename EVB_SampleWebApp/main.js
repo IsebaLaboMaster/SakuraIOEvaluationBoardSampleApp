d0fa546c55791e82a9b6481658d06d9878fc38e0
IsebaLaboMaster <38211043+IsebaLaboMaster@users.noreply.github.com>
2018-05-15 12:10:54 +0900

    Add files via upload

--- EVB_SampleWebApp/main.js
+++ EVB_SampleWebApp/main.js
@@ -15,13 +15,35 @@ window.onload = function() {
   mmd.registerKeyListener(document);
   mmd.registerMouseListener(document);
   
-  
-  
+  //(IsebaLaboMaster追記コメントここから)
+  //
+  //下記ソースコード
+  //「var miku = new MMD.Model('model', 'Miku_Hatsune_Ver2.pmd');」
+  //のうち、「Miku_Hatsune_Ver2.pmd」のファイル名を以下のものに変更すると
+  //MMDモデルを変更することができます。
+  //
+  //「Haku_Yowane.pmd」
+  //「KAITO.pmd」
+  //「Len_Kagamine.pmd」
+  //「MEIKO.pmd」
+  //「Meiko_Sakine.pmd」
+  //「Miku_Hatsune.pmd」
+  //「Miku_Hatsune_metal.pmd」
+  //「Miku_Hatsune_Ver2.pmd」
+  //「Neru_Akita.pmd」
+  //「Rin_Kagamene_act2.pmd」
+  //「Rin_Kagamine.pmd」
+  //
+  //※これら変更はMMDモデルデータの扱いおよびプログラムコード編集の知識がある方推奨です。
+  //
+  //(IsebaLaboMaster追記コメントここまで)
   var miku = new MMD.Model('model', 'Miku_Hatsune_Ver2.pmd');
+  
   //var miku = new MMD.Model('Lat', 'Normal.pmd');
   //var miku = new MMD.Model('mobko', 'mobko.pmd');
   //var miku = new MMD.Model('yufu', 'yufu.pmd');
   //var miku = new MMD.Model('defoko', 'defoko.pmd');
+  
   miku.load(function() {
     mmd.addModel(miku);
     mmd.initBuffers();
